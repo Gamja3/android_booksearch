4a9d11b05ec6d1bf00c8c27dc28979bc91bf3750
Gamja3 <44pico@naver.com>
2023-06-18 23:10:05 +0900

    6/18 노트북

--- app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/example/booksearchapp/ui/view/SearchFragment.kt
@@ -1,17 +1,26 @@
 package com.example.booksearchapp.ui.view
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.booksearchapp.databinding.FragmentSearchBinding
+import com.example.booksearchapp.ui.adapter.BookSearchAdapter
+import com.example.booksearchapp.ui.viewmodel.BookSearchViewModel
+import com.example.booksearchapp.util.Constants.SEARCH_BOOKS_TIME_DELAY
 
 
-
-    class SearchFragment : Fragment(){
+class SearchFragment : Fragment(){
         private var _binding : FragmentSearchBinding? = null
         private val binding get() = _binding!!
+
+        private lateinit var bookSearchViewModel: BookSearchViewModel
+        private lateinit var bookSearchAdapter: BookSearchAdapter
 
         override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,6 +30,41 @@ import com.example.booksearchapp.databinding.FragmentSearchBinding
             _binding = FragmentSearchBinding.inflate(inflater,container, false)
             return binding.root
         }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bookSearchViewModel = (activity as MainActivity).bookSearchViewModel
+
+        setupRecycleView()
+    }
+
+    private fun setupRecycleView(){
+        bookSearchAdapter = BookSearchAdapter()
+        binding.rvSearchResult.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+            adapter = bookSearchAdapter
+        }
+    }
+
+    private fun searchBooks(){
+        var startTime = System.currentTimeMillis()
+        var endTime: Long
+
+        binding.etSearch.addTextChangedListener { text : Editable? ->
+            endTime = System.currentTimeMillis()
+            if(endTime - startTime >= SEARCH_BOOKS_TIME_DELAY){
+                text?.let {
+                    var query = it.toString().trim()
+                    if(query.isNotEmpty()){
+                        bookSearchViewModel.searchBooks(query)
+                    }
+                }
+            }
+            startTime = endTime
+        }
+    }
 
         override fun onDestroyView() {
             _binding = null
